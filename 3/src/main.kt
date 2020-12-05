cdc735223a7de8cb05c64b50508779585ce97922
Priit Trink <dusttland@gmail.com>
2020-12-05 23:20:10 +0200

    Modification in 3.

--- 3/src/main.kt
+++ 3/src/main.kt
@@ -1,11 +1,12 @@
 import java.io.File
 
 typealias Trees = Set<Location>
+typealias Step = Location
 
 data class Size(val width: Int, val height: Int)
 
 data class Location(val x: Int, val y: Int) {
-    operator fun plus(size: Size) = Location(this.x + size.width, this.y + size.height)
+    operator fun plus(other: Location) = Location(this.x + other.x, this.y + other.y)
     operator fun rem(size: Size) = Location(this.x % size.width, this.y % size.height)
 }
 
@@ -19,7 +20,7 @@ class Map(val size: Size, val trees: Trees) {
         return this.trees.contains(adjustedLocation)
     }
 
-    fun countAccordingToRules(step: Size): Long {
+    fun countAccordingToRules(step: Step): Long {
         var location = Location(0, 0)
         var count = 0L
         while (true) {
@@ -43,7 +44,7 @@ fun String.parseMap(): Map {
     return Map(size, trees)
 }
 
-fun printAnswer(map: Map, steps: List<Size>) {
+fun printAnswer(map: Map, steps: List<Step>) {
     val answer: Long = steps
         .map { step -> map.countAccordingToRules(step) }
         .fold(1L) { acc, count -> acc * count }
@@ -56,15 +57,15 @@ fun main() {
 
     println("--- Day 3: Toboggan Trajectory ---")
     printAnswer(map = map, steps = listOf(
-        Size(3, 1))
+        Step(3, 1))
     )
 
     println("--- Part Two ---")
     printAnswer(map = map, steps = listOf(
-        Size(1, 1),
-        Size(3, 1),
-        Size(5, 1),
-        Size(7, 1),
-        Size(1, 2)
+        Step(1, 1),
+        Step(3, 1),
+        Step(5, 1),
+        Step(7, 1),
+        Step(1, 2)
     ))
 }
