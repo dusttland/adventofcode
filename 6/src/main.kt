b4aa543f2996bf874bf05fdda6fa3e0b7bb4d875
Priit Trink <dusttland@gmail.com>
2020-12-06 17:11:50 +0200

    Use of intersect method.

--- 6/src/main.kt
+++ 6/src/main.kt
@@ -2,15 +2,11 @@ import java.io.File
 
 fun main() {
     val data: String = File("input.txt").readText()
-    val groups: List<List<Set<Char>>> = data
-        .split("\n\n")
-        .map { persons: String ->
-            persons
-                .split('\n')
-                .map { answers: String ->
-                    answers.fold(mutableSetOf<Char>()) { acc, answer: Char -> acc.add(answer); acc }
-                }
+    val groups: List<List<Set<Char>>> = data.split("\n\n").map { persons: String ->
+        persons.split('\n').map { answers: String ->
+            answers.fold(mutableSetOf<Char>()) { acc, answer: Char -> acc.add(answer); acc }
         }
+    }
 
     val answer1: Int = groups
         .map { persons: List<Set<Char>> ->
@@ -20,11 +16,9 @@ fun main() {
 
     val answer2: Int = groups
         .map { persons: List<Set<Char>> ->
-            persons
-                .fold(mutableSetOf<Char>()) { acc, answers: Set<Char> -> acc.addAll(answers); acc }
-                .filter { answer: Char -> persons.all { answers: Set<Char> -> answers.contains(answer)} }
+            persons.fold(persons.first()) { acc, answers: Set<Char> -> acc.intersect(answers) }
         }
-        .fold(0) { acc, list: List<Char> -> acc + list.size }
+        .fold(0) { acc, set: Set<Char> -> acc + set.size }
 
     println("--- Day 6: Custom Customs ---")
     println(answer1)
