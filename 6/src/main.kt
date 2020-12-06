b29628c132a9155763f7c5b8f087fe6dbd20ea36
Priit Trink <dusttland@gmail.com>
2020-12-06 16:11:07 +0200

    Solution of day 6.

---
+++ 6/src/main.kt
@@ -0,0 +1,33 @@
+import java.io.File
+
+fun main() {
+    val data: String = File("input.txt").readText()
+    val groups: List<List<Set<Char>>> = data
+        .split("\n\n")
+        .map { persons ->
+            persons
+                .split('\n')
+                .map { answers: String ->
+                    answers.fold(mutableSetOf<Char>()) { acc, answer: Char -> acc.add(answer); acc }
+                }
+        }
+
+    val answer1: Int = groups
+        .map { persons: List<Set<Char>> ->
+            persons.fold(mutableSetOf<Char>()) { acc, answers: Set<Char> -> acc.addAll(answers); acc }
+        }
+        .fold(0) { acc, list -> acc + list.size }
+
+    val answer2: Int = groups
+        .map { persons: List<Set<Char>> ->
+            persons
+                .fold(mutableSetOf<Char>()) { acc, answers: Set<Char> -> acc.addAll(answers); acc }
+                .filter { answer: Char -> persons.all { answers: Set<Char> -> answers.contains(answer)} }
+        }
+        .fold(0) { acc, list -> acc + list.size }
+
+    println("--- Day 6: Custom Customs ---")
+    println(answer1)
+    println("--- Part Two ---")
+    println(answer2)
+}
