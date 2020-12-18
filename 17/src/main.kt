c18e8cb5995f9475bd290b54aed89ebe5aa7e47b
Priit Trink <priit.trink@gmail.com>
2020-12-18 20:24:17 +0200

    Solution of day 17.

--- 17/src/main.kt
+++ 17/src/main.kt
@@ -1,133 +1,73 @@
 import java.io.File
 
-typealias Room = List<List<List<Boolean>>>
+typealias Coords = List<Int>
+typealias ActiveCubes = Set<Coords>
 
-fun Room.value(c: Coords): Boolean = this[c.z][c.y][c.x]
+fun Coords.withMutableNeighbours(): MutableSet<Coords> {
+    if (this.isEmpty()) return mutableSetOf(listOf(), listOf(), listOf())
+    val childDimensionNeighbours = this.subList(1, this.size).withMutableNeighbours()
+    val neighbours = mutableSetOf<Coords>()
+    for (i in -1..1) {
+        childDimensionNeighbours.forEach {
+            val newCoords = mutableListOf(this[0] + i)
+            newCoords.addAll(it)
+            neighbours.add(newCoords)
+        }
+    }
+    return neighbours
+}
 
-val Room.coordsSize: Coords
-    get() = Coords(this[0][0].size, this[0].size, this.size)
+fun Coords.neighbours(): Set<Coords> {
+    val neighbours: MutableSet<Coords> = this.withMutableNeighbours()
+    neighbours.remove(this)
+    return neighbours
+}
 
-fun Room.forEachCoord(block: (Coords, Boolean) -> Unit) {
-    for (z in this.indices) {
-        for (y in this[z].indices) {
-            for (x in this[z][y].indices) {
-                val coords = Coords(x, y, z)
-                block.invoke(coords, this.value(coords))
+fun Coords.withNeighbours(): Set<Coords> = this.withMutableNeighbours()
+
+fun ActiveCubes.isActive(coords: Coords): Boolean = this.contains(coords)
+
+fun ActiveCubes.coordsToCheck(): Set<Coords> {
+    return this.fold(mutableSetOf()) { acc, coords -> acc.addAll(coords.withNeighbours()); acc }
+}
+
+fun ActiveCubes.cycled(): ActiveCubes {
+    return this.coordsToCheck().fold(mutableSetOf()) { acc, coords ->
+        val activeCount = coords.neighbours().count { this.isActive(it) }
+        val isActive = this.isActive(coords)
+        when {
+            isActive && (activeCount == 2 || activeCount == 3) -> acc.add(coords)
+            !isActive && activeCount == 3 -> acc.add(coords)
+        }
+        acc
+    }
+}
+
+fun String.parseActiveCubes(dimensions: Int): ActiveCubes {
+    val activeCubes = mutableSetOf<Coords>()
+    val lines = this.split('\n')
+    lines.forEachIndexed { lineIndex, line ->
+        line.forEachIndexed { index, char ->
+            if (char == '#') {
+                val coords = mutableListOf<Int>()
+                repeat(dimensions - 2) { coords.add(0) }
+                coords.add(lineIndex)
+                coords.add(index)
+                activeCubes.add(coords)
             }
         }
     }
-}
-
-fun Room.count(block: (Boolean) -> Boolean): Int {
-    var count = 0
-    for (z in this.indices) {
-        for (y in this[z].indices) {
-            for (x in this[z][y].indices) {
-                val coords = Coords(x, y, z)
-                if (block(this.value(coords))) count++
-            }
-        }
-    }
-    return count
-}
-
-fun Room.changed(block: (Room, Coords, Boolean) -> Boolean): Room {
-    val size = this.coordsSize
-    return List(size.z) { z ->
-        List(size.y) { y ->
-            List(size.x) { x ->
-                val coords = Coords(x, y, z)
-                block(this, coords, this.value(coords))
-            }
-        }
-    }
-}
-
-fun Room.transform(coords: Coords): Room {
-    val size = this.coordsSize
-    return List(coords.z + size.z) { z ->
-        List(coords.y + size.y) { y ->
-            List(coords.x + size.x) { x ->
-                val oldCoords = Coords(x, y, z) - coords
-                this.valueOrFalse(oldCoords)
-            }
-        }
-    }
-}
-
-fun Room.expand(coords: Coords): Room {
-    val size = this.coordsSize
-    return List(2 * coords.z + size.z) { z ->
-        List(2 * coords.y + size.y) { y ->
-            List(2 * coords.x + size.x) { x ->
-                val oldCoords = Coords(x, y, z) - coords
-                this.valueOrFalse(oldCoords)
-            }
-        }
-    }
-}
-
-infix fun Room.contains(c: Coords): Boolean {
-    return c.z in this.indices && c.y in this[c.z].indices && c.x in this[c.z][c.y].indices
-}
-
-fun Room.prettyPrint() {
-    this.forEach { z ->
-        z.forEach { y ->
-            y.forEach { x -> val c = if (x) '#' else '.'; print(c) }
-            println()
-        }
-        println()
-    }
-}
-
-infix fun Coords.isIn(room: Room) = room contains this
-
-fun Room.valueOrNull(coords: Coords): Boolean? = when {
-    coords isIn this -> this.value(coords)
-    else -> null
-}
-
-fun Room.valueOrFalse(coords: Coords): Boolean = this.valueOrNull(coords) ?: false
-
-data class Coords(val x: Int, val y: Int, val z: Int) {
-    override fun toString(): String = "(${this.x},${this.y},${this.z})"
-    operator fun plus(other: Coords) = Coords(this.x + other.x, this.y + other.y, this.z + other.z)
-    operator fun minus(other: Coords) = Coords(this.x - other.x, this.y - other.y, this.z - other.z)
-    fun neighbours(): Set<Coords> {
-        val set = mutableSetOf<Coords>()
-        for (z in -1..1) {
-            for (y in -1..1) {
-                for (x in -1..1) {
-                    set.add(Coords(this.x - x, this.y - y, this.z - z))
-                }
-            }
-        }
-        set.remove(this)
-        return set
-    }
-}
-
-fun String.parseRoom(): Room = listOf(this
-    .split('\n')
-    .map { charList -> charList.trim().map { char -> char == '#' } }
-)
-
-fun Room.cycled(): Room = this.expand(Coords(1, 1, 1)).changed { old, coords, value ->
-    val nCount = coords.neighbours().count { old.valueOrFalse(it) }
-    when {
-        value && (nCount == 2 || nCount == 3) -> true
-        !value && nCount == 3 -> true
-        else -> false
-    }
+    return activeCubes
 }
 
 fun main() {
     val data: String = File("input.txt").readText()
-    val room: Room = data.parseRoom()
 
+    var cubes: ActiveCubes = data.parseActiveCubes(dimensions = 3)
+    repeat(6) { cubes = cubes.cycled() }
+    println("Part 1: ${cubes.size}")
 
-    var cycledRoom: Room = room
-    repeat(6) { cycledRoom = cycledRoom.cycled() }
-    println("Part 1: ${cycledRoom.count { it }}")
+    cubes = data.parseActiveCubes(dimensions = 4)
+    repeat(6) { cubes = cubes.cycled() }
+    println("Part 2: ${cubes.size}")
 }
