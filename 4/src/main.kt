9b42ea5a9de89d04593660f1c2515f2ae35d565f
Priit Trink <dusttland@gmail.com>
2021-01-18 22:28:15 +0200

    Tuned 4.

--- 4/src/main.kt
+++ 4/src/main.kt
@@ -1,80 +1,61 @@
 import java.io.File
 
-typealias PassportCandidate = Map<String, String>
-typealias ParameterValidators = Map<String, (String) -> Boolean>
+typealias Passport = Map<String, String>
 
-val eyeColors = setOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
+class FieldWithValidator(val field: String, private val validator: (String) -> Boolean) {
+    fun isValid(value: String): Boolean = tryOrFalse { this.validator.invoke(value) }
+}
 
-val requiredParameters: ParameterValidators = mapOf(
-    "byr" to { value ->
-        try { value.toInt() in 1920..2002 }
-        catch (e: Exception) { false }
-    },
-    "iyr" to { value ->
-        try { value.toInt() in 2010..2020 }
-        catch (e: Exception) { false }
-    },
-    "eyr" to { value ->
-        try { value.toInt() in 2020..2030 }
-        catch (e: Exception) { false }
-    },
-    "ecl" to { value -> eyeColors.contains(value) },
-    "pid" to { value -> Regex("[0-9]{9}").matches(value) },
-    "hcl" to { value -> Regex("#[a-f0-9]{6}").matches(value) },
-    "hgt" to { value ->
+val EYE_COLORS = setOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
+
+val REQUIRED_FIELDS: List<FieldWithValidator> = listOf(
+    "byr" validWhen { it.toInt() in 1920..2002 },
+    "iyr" validWhen { it.toInt() in 2010..2020 },
+    "eyr" validWhen { it.toInt() in 2020..2030 },
+    "ecl" validWhen { it in EYE_COLORS },
+    "pid" validWhen { it matches "[0-9]{9}" },
+    "hcl" validWhen { it matches "#[a-f0-9]{6}" },
+    "hgt" validWhen {
         when {
-            value.contains("cm") -> {
-                try { value.removeSuffix("cm").toInt() in 150..193 }
-                catch (e: Exception) { false }
-            }
-            value.contains("in") -> {
-                try { value.removeSuffix("in").toInt() in 59..76 }
-                catch (e: Exception) { false }
-            }
+            it.contains("cm") -> it.removeSuffix("cm").toInt() in 150..193
+            it.contains("in") -> it.removeSuffix("in").toInt() in 59..76
             else -> false
         }
     }
 )
 
-fun PassportCandidate.isValid1(): Boolean {
-    for ((paramName, _) in requiredParameters) {
-        this[paramName] ?: return false
-    }
-    return true
+inline fun tryOrFalse(operation: () -> Boolean): Boolean = try { operation() } catch (e: Exception) { false }
+
+infix fun String.validWhen(validator: (String) -> Boolean) = FieldWithValidator(this, validator)
+
+infix fun String.matches(regexString: String) = regexString.toRegex().matches(this)
+
+infix fun FieldWithValidator.isValidIn(passport: Passport): Boolean {
+    val value = passport[this.field] ?: return false
+    return this.isValid(value)
 }
 
-fun PassportCandidate.isValid2(): Boolean {
-    for ((paramName, validationFunction) in requiredParameters) {
-        val value = this[paramName] ?: return false
-        if (!validationFunction.invoke(value)) return false
-    }
-    return true
-}
+fun Passport.containsAllRequiredFields(): Boolean = REQUIRED_FIELDS.all { it.field in this }
+
+fun Passport.isValid(): Boolean = REQUIRED_FIELDS.all { it isValidIn this }
 
 fun String.parseParameter(): Pair<String, String> {
     val values = this.split(':')
     return values[0] to values[1]
 }
 
-fun String.parsePassportCandidate(): PassportCandidate = this
+fun String.parsePassportCandidate(): Passport = this
     .split(' ', '\n')
     .filter { it.isNotBlank() }
     .map { it.parseParameter() }
     .toMap()
 
-fun String.parsePassportCandidates(): List<PassportCandidate> = this
+fun String.parsePassportCandidates(): List<Passport> = this
     .split("\n\n")
     .map { it.parsePassportCandidate() }
 
 fun main() {
-    val data: String = File("input.txt").readText()
-    val passports: List<PassportCandidate> = data.parsePassportCandidates()
-
-    println("--- Day 4: Passport Processing ---")
-    val count1: Int = passports.count { it.isValid1() }
-    println(count1)
-
-    println("--- Part Two ---")
-    val count2: Int = passports.count { it.isValid2() }
-    println(count2)
+    val passports: List<Passport> = File("input.txt").readText().parsePassportCandidates()
+    println("Part 1: ${passports.count { it.containsAllRequiredFields() }}")
+    println("Part 2: ${passports.count { it.isValid() }}")
 }
